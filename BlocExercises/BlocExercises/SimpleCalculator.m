e2431b36f5f5c99574be1f549a116fd01780e7c6
Stephen Kaiser <stephenkais@gmail.com>
2016-06-15 20:36:59 -0700

    Assignment 8 completed

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -11,18 +11,18 @@
 @implementation SimpleCalculator
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
-    /* WORK HERE */
-    return number;
+    NSInteger stephen = number + 1;
+    return stephen;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    /* WORK HERE */
-    return number1;
+    NSInteger sum = number1 + number2;
+    return sum;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
-    /* WORK HERE */
-    return 0;
+    NSInteger remainder = (dividend % divisor);
+    return remainder;
 }
 
 @end
