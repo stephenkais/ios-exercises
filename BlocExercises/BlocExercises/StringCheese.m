6daa4db4f0245574f303beba809d0b631c1f62b2
Stephen Kaiser <stephenkais@gmail.com>
2016-06-21 19:43:51 -0700

    Assignment 9 completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,8 +11,9 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSString *favestring = @"My favorite cheese is ";
+    NSString *return_value = [favestring stringByAppendingFormat:@"%@.", cheeseName];
+    return return_value;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
@@ -22,9 +23,17 @@
      (You will learn more about if/else statements in a later checkpoint.)
      */
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
-        /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
-    } else {
-        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+            NSString *return_value = [cheeseName stringByReplacingOccurrencesOfString:@" cheese" withString:@""];
+        if ([return_value length] == [cheeseName length]){
+            
+            return_value = [cheeseName stringByReplacingOccurrencesOfString:@" Cheese" withString:@""];
+            return return_value;
+        } else {
+            return return_value;
+        }
+        } else {
+        NSString *return_value = cheeseName;
+        return return_value;
     }
 
     /*
@@ -39,14 +48,15 @@
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        NSString *cheeses = [NSString stringWithFormat:@"%li", (long)cheeseCount];
+        NSString *return_value = [cheeses stringByAppendingString:@" cheese"];
+        return return_value;
+        
     } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSString *cheeses = [NSString stringWithFormat:@"%li", (long)cheeseCount];
+        NSString *return_value = [cheeses stringByAppendingString:@" cheeses"];
+        return return_value;
     }
-    
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
     
     return nil;
 }
