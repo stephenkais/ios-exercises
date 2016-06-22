711026c49cbc37dd5ee067ff3be35de9e441dfe3
Stephen Kaiser <stephenkais@gmail.com>
2016-06-21 21:54:48 -0700

    Assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,22 +11,32 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
+    NSArray *starTrekArray = [characterString componentsSeparatedByString:@";"];
+    return starTrekArray;
     return @[];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    NSMutableArray *characterMutableArray = [NSMutableArray arrayWithArray:characterArray];
+    NSString *starTrekkies = [characterMutableArray componentsJoinedByString:@";"];
+    return starTrekkies;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *characterMutableArray = [NSMutableArray arrayWithArray:characterArray];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [characterMutableArray sortUsingDescriptors:@[sortDescriptor]];
+    return characterMutableArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
+    NSMutableArray *characterMutableArray = [NSMutableArray arrayWithArray:characterArray];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [characterMutableArray filterUsingPredicate:containsWorf];
+    if ([characterMutableArray count] == 0){
+        return NO; } else {
+            return YES;
+        }
     return NO;
 }
 
